bcf7885376b4fcbe0e75e845e1cba4f94cec6fa1
Dasha Lary <dasha4988@gmail.com>
2021-05-28 15:50:54 -0400

    included standard library

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stddef.h>
 
 int main(void)
@@ -15,7 +16,7 @@ int main(void)
     printf("pnum's address: %p\n", (void *)&pnum);   // outputs the address
     printf("pnum's size: %d bytes\n", sizeof(pnum)); // outputs the size
     printf("pnum's value: %p\n", pnum);              // outputs the value
-    printf("value pointed to: %d\n", *pnum);         // value at the address that is at the value of pointer
+    printf("value pointed to: %d\n\n", *pnum);       // value at the address that is at the value of pointer
 
     return 0;
 }
