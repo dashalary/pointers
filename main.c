f7ee0545b68518c4e41c057e78be87fb8d9d3c80
Dasha Lary <dasha4988@gmail.com>
2021-05-28 15:47:54 -0400

    added comments

--- main.c
+++ main.c
@@ -1,4 +1,21 @@
+#include <stdio.h>
+#include <stddef.h>
+
 int main(void)
 {
+    int num = 0;
+    int *pnum = NULL; // a pointer that points to type int
+
+    num = 10;
+    printf("num's address: %p\n", &num); // outputs the address
+    printf("num's value: %d\n\n", num);  // outputs the value
+
+    pnum = &num; // stores the address of num in pnum
+
+    printf("pnum's address: %p\n", (void *)&pnum);   // outputs the address
+    printf("pnum's size: %d bytes\n", sizeof(pnum)); // outputs the size
+    printf("pnum's value: %p\n", pnum);              // outputs the value
+    printf("value pointed to: %d\n", *pnum);         // value at the address that is at the value of pointer
+
     return 0;
 }
